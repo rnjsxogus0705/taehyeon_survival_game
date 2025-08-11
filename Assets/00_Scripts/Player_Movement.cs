92210443a9bdc964cea067d78a2672e38b5c5610
rnjsxogus0705 <rnjsxogus0705@gmail.com>
2025-08-11 20:07:21 +0900

    챕터2 완료
    플레이어의 움직임과 카메라의 움직임 구현

---
+++ Assets/00_Scripts/Player_Movement.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player_Movement : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public Vector3 cameraDir = Vector3.zero;
+
+    private Camera m_camera;
+    private Vector3 moveDir;
+    private CharacterController controller;
+    private Animator animator;
+
+    private void Start()
+    {
+        m_camera = Camera.main;
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Move();
+        Rotate();
+        Animate();
+        CameraMove();
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector3(h, 0, v).normalized;
+        controller.SimpleMove(moveDir * moveSpeed);
+    }
+
+    void Rotate()
+    {
+        if (moveDir.sqrMagnitude > 0.01f)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(moveDir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, 10f * Time.deltaTime);
+        }
+    }
+
+    void Animate()
+    {
+        animator.SetFloat("SPEED", moveDir.magnitude);
+    }
+
+    void CameraMove()
+    {
+        m_camera.transform.position = Vector3.Lerp(
+            m_camera.transform.position,
+            transform.position + cameraDir, 
+            2.0f * Time.deltaTime);
+    }
+}
